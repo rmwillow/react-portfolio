d8ef981e2f1938626fbefef82877b1312f702eb7
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-09-13 18:36:50 -0500

    social media and contact links

---
+++ src/component/footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import codepen from "../img/codepen.png";
+import github from "../img/github.png";
+import linkedin from "../img/linkedin.png";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer" className="container-fluid text-center py-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-sm-12 col-md-8 mx-auto">
+                            <a
+                                href="https://stackoverflow.com/users/15362075/rachel"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <img className="img-fluid" src={codepen} alt=""/>
+                            </a>
+                            <a
+                                className="px-3"
+                                href="https://github.com/rmwillow"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <img src={github} alt=""/>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/rachel-mcgrath-605a361b8/"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <img src={linkedin} alt=""/>
+                            </a>
+                        </div>
+                    </div>
+                    <h5 className="pt-4"><p style="text-align: center"> Copyright &copy; 2021-<script>document.write(new Date().getFullYear())</script> Rachel McGrath All Rights Reserved</p></h5>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
